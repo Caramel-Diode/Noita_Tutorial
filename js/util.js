8696b5fb6df48ee5cd6dce324c8b4e3a32f30bad
bdtgd <2746671038@qq.com>
2024-07-07 04:44:41 +0800

    *

--- js/util.js
+++ js/util.js
@@ -1,12 +1,13 @@
+noitaLib("dev");
 let fragment = document.createDocumentFragment();
 const appendSpellElements = exp => {
-    const arr = noitaLib.spell.queryByExp(exp);
-    for (let i = 0; i < arr.length; i++) fragment.append(new noitaLib.spell([arr[i]]));
+    const arr = noitaLib.Spell.queryByExp(exp);
+    for (let i = 0; i < arr.length; i++) fragment.append(new noitaLib.Spell([arr[i]]));
 };
 
 const appendSpellElementsWithSort = exp => {
-    const arr = noitaLib.spell.queryByExp(exp);
+    const arr = noitaLib.Spell.queryByExp(exp);
     const prop = "prob_lv" + exp.match(/[0-9]+/)[0];
-    arr.sort((a, b) => b.spawningData[prop] - a.spawningData[prop]);
-    for (let i = 0; i < arr.length; i++) fragment.append(new noitaLib.spell([arr[i]]));
+    arr.sort((a, b) => b.spawn[prop] - a.spawn[prop]);
+    for (let i = 0; i < arr.length; i++) fragment.append(new noitaLib.Spell([arr[i]]));
 };
